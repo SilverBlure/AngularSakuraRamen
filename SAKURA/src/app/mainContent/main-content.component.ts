fa9cea036d6a0f9259802c3aa8e28e18e33b2e4a
Nicolai Österle <oesterle.ni@onlinehome.de>
2025-10-19 16:45:02 +0200

    design Page

--- SAKURA/src/app/mainContent/main-content.component.ts
+++ SAKURA/src/app/mainContent/main-content.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { LandingPageComponent } from './landingPage/landing-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent],
-  templateUrl: './main-content.component.html',
+  imports: [NavbarComponent, HowToOrderComponent],
+  template: `
+  <div class="backgroundImage">
+  <div class="navbar">
+  <app-navbar></app-navbar></div>
+  <h1>SAKURA RAMEN</h1>
+  <h2>BEST RAMEN IN TOWN</h2></div>
+  <app-how-to-order></app-how-to-order>
+
+  `,
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
